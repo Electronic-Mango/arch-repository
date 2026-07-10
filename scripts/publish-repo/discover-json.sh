5924cde50e004dfc3955772c5757ab7dc7ded49b
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-07-10 22:45:45 +0200

    Remove "replace" from publish-repo.yml
    Instead add artifact name into JSON input.

--- scripts/publish-repo/discover-json.sh
+++ scripts/publish-repo/discover-json.sh
@@ -16,4 +16,6 @@ if [[ ${#pkg_dirs[@]} -eq 0 ]]; then
   exit 1
 fi
 
-printf '%s\n' "${pkg_dirs[@]}" | jq -R '{package_dir: .}' | jq -c -s .
+printf '%s\n' "${pkg_dirs[@]}" |
+  jq -R '{package_dir: ., artifact_suffix: (. | gsub("/"; "-"))}' |
+  jq -c -s .
