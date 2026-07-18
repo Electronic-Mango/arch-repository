0a3f478b5e16ab27a7736a38557adae4572c80b1
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-07-18 17:34:09 +0200

    proton-ge-custom: update .SRCINFO in the update script

--- scripts/update/proton-ge-custom.sh
+++ scripts/update/proton-ge-custom.sh
@@ -12,17 +12,26 @@ fi
 package_name="proton-ge-custom"
 updates_api_url="https://api.github.com/repos/GloriousEggroll/${package_name}/releases/latest"
 
+cd "${packages_dir}/${package_name}"
+
 api_response=$(curl -sL --max-time 10 "${updates_api_url}")
 version=$(echo "${api_response}" | jq -r '.tag_name')
-sha_url=$(echo "${api_response}" | jq -r --arg name "${version}.sha512sum" '.assets[] | select(.name == $name) | .browser_download_url')
-sha512hash=$(curl -sL --max-time 10 "${sha_url}" | awk '{print $1}')
-
-cd "${packages_dir}/${package_name}"
 
 if grep -q "_pkgver=${version}" PKGBUILD; then
     echo "PKGBUILD is already up to date with version ${version}."
     exit 0
 fi
 
-sed -i "s/^_pkgver=.*/_pkgver=${version}/" PKGBUILD
-sed -i "/^sha512sums=(/{n;s/.*/    '${sha512hash}'/;}" PKGBUILD
+sha_url=$(echo "${api_response}" | jq -r --arg name "${version}.sha512sum" '.assets[] | select(.name == $name) | .browser_download_url')
+sha512hash=$(curl -sL --max-time 10 "${sha_url}" | awk '{print $1}')
+
+source PKGBUILD
+old_version="${_pkgver}"
+old_sha512hash="${sha512sums[0]}"
+
+sed -i "s/${old_version}/${version}/g" PKGBUILD
+sed -i "s/${old_sha512hash}/${sha512hash}/" PKGBUILD
+
+sed -i "s/${old_version}/${version}/g" .SRCINFO
+sed -i "s/${old_sha512hash}/${sha512hash}/" .SRCINFO
+sed -i "s/${old_version//-/_}/${version//-/_}/g" .SRCINFO
