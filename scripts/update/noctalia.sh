2287bbb2ea7b35f8ed338e039f83003a65049bba
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-08-14 20:55:34 +0200

    noctalia: configure "provides" and "conflicts" in update script

--- scripts/update/noctalia.sh
+++ scripts/update/noctalia.sh
@@ -22,3 +22,15 @@ shopt -s dotglob
 rm -rf -- ./*
 cp -a "${tmp_repo_dir}"/. .
 rm -rf "${tmp_repo_dir}"
+
+if [[ $(tail -n 1 PKGBUILD) != "" ]]; then
+    echo >> PKGBUILD
+fi
+
+if ! grep -q "^provides=" PKGBUILD; then
+    echo 'provides=('noctalia')' >> PKGBUILD
+fi
+
+if ! grep -q "^conflicts=" PKGBUILD; then
+    echo 'conflicts=('noctalia' 'noctalia-bin' 'noctalia-git' 'noctalia-shell' 'noctalia-meta')' >> PKGBUILD
+fi
